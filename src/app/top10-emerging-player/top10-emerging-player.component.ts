ec07b38e4989f08ffccca1171f2c4bc78d0d075c
shubham karad <shubham.karad12@gmail.com>
2023-03-03 16:44:34 +0530

    changes done for Production

--- src/app/top10-emerging-player/top10-emerging-player.component.ts
+++ src/app/top10-emerging-player/top10-emerging-player.component.ts
@@ -1,4 +1,4 @@
-import { Component , OnInit } from '@angular/core';
+import { Component , OnInit,ElementRef } from '@angular/core';
 import { Location } from '@angular/common';
 import { FullscreenServiceService } from '../fullscreen-service.service';
 import { ActivatedRoute } from '@angular/router';
@@ -22,9 +22,10 @@ export class Top10EmergingPlayerComponent {
   gameid: any;
   apiid: any;
   _gameId: any;
+  points_color: any;
 
   
-  constructor(public location:Location,public http:FullscreenServiceService,public router:ActivatedRoute){
+  constructor(public location:Location,public http:FullscreenServiceService,public router:ActivatedRoute,public element:ElementRef){
    
 
   }
@@ -60,8 +61,12 @@ ngOnInit(): void {
     console.log(res);
     this.apiid=localStorage.getItem('id');
     console.log(this.apiid);
+    this.points_color=res.data.fs_details[0]._fullscreen_themedetails.bg_color
+    this.element.nativeElement.style.setProperty('--myvar', this.points_color)
     if(this.apiid==='undefined'){
       this.themeDetails=res.data.fs_details[0]._fullscreen_themedetails;
+    
+
       this.label=res.data.fs_details[0].label;
     this.ranking_data=res.data.fs_details[0]._ranking_data;
     setInterval(() => {
